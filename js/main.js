86c69b2e27f9a01323579b196b1ddc194deb1095
AnselOne1 <77886022+AnselOne1@users.noreply.github.com>
2021-03-14 00:13:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+
+
+$('.feedback__slider').slick({
+
+    fade: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    swipe: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    cssEase: 'linear',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="img/arrow-right.svg" alt=""></button>'
+
+});
+
+
+
+});
